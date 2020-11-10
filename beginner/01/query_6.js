b2bc2d40d1cc5d6c8dc91fa926e189db11e435da
MT2007 <53901460+MT2007@users.noreply.github.com>
2020-11-10 22:36:03 +0100

    Add files via upload

---
+++ beginner/01/query_6.js
@@ -0,0 +1,29 @@
+printjson(db.people.insert({
+        "sex" : "Male",
+        "first_name" : "Mohammed",
+        "last_name" : "Al-Saeed",
+        "job" : "Software Engineer",
+        "email" : "m.al-saeed@gmail.com",
+        "location" : {
+                "city" : "Warszawa",
+                "address" : {
+                        "streetname" : "Beilfuss",
+                        "streetnumber" : "860"
+                }
+        },
+        "description" : "vulputate justo in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris",
+        "height" : "182.38",
+        "weight" : "78.81",
+        "birth_date" : "1982-01-06T02:55:03Z",
+        "nationality" : "Iraq",
+        "credit" : [
+                {
+                        "type" : "switch",
+                        "number" : "6759888939100098699",
+                        "currency" : "PLN",
+                        "balance" : "5117.06"
+                }
+        ]
+}
+)
+)
